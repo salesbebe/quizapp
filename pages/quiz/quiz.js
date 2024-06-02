b311ca35147f3e431e2d049261198b719711f8fa
brendinha gemaplys <sales.brenda@eaportal.org>
2024-06-02 13:41:18 +0000

    adicionada as funções do botão de próxima pergunta e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -97,7 +97,7 @@ function montarPergunta(){
        
     </form>
 
-    <button>Enviar</button>
+    <button>Responder</button>
 </section>`
 }
 function alterarSinais(texto){
@@ -113,6 +113,20 @@ function guardarResposta(evento){
 }
 
 function validarResposta(){
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click",validarResposta)
+   
+
+
+    if (pergunta === 10){
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click",finalizar)
+    }
+    else{
+        botaoEnviar.addEventListener("click",proximaPergunta)
+    }
+
     if (resposta === quiz.questions[pergunta-1].answer){
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id","correta")
         pontos = pontos + 1
@@ -122,12 +136,20 @@ function validarResposta(){
         document.querySelector(`label[for=${respostaCorretaId}]`).setAttribute("id","correta")
 
     }
+    pergunta = pergunta + 1
 }
-async function iniciar() {
-    alterarAssunto()
-    await buscarPerguntas()
-    montarPergunta()
 
+function finalizar(){
+    localStorage.setItem("pontos", pontos)
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta(){
+    montarPergunta()
+    adicionarEventosInputs()
+}
+
+function adicionarEventosInputs(){
     const inputsResposta =  document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click",guardarResposta)
@@ -136,6 +158,14 @@ async function iniciar() {
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar() {
+    alterarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+    adicionarEventosInputs()
+
 }
 
 iniciar()
